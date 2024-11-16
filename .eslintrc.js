c3ef3a0228499e2f15382736ac7fab8a0b17c4ce
Boyan Damyanov <boyan.damyanov@endurosat.com>
2024-11-16 18:50:37 +0200

    fix build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    extends: [
-      'airbnb',
-      'airbnb/hooks',
-      'plugin:@typescript-eslint/recommended',
-      'prettier'
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    env: {
-      browser: true,
-      es2021: true,
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {
-      // Add custom rules here
-    },
-  };
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+};
